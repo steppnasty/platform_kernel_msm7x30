b2066c5bd1152813921a9a81b10084275854f8e2
Rahul Ruikar <rahul.ruikar@gmail.com>
2010-10-28 17:31:19 +0200

    USB: gadget: f_mass_storage: put_device() in error recovery
    This commit fixes an issue with error recovery after
    device_register() fails in Mass Storage Function.  The device
    needs to be put to avoid resource leakage.
    
    Signed-off-by: Rahul Ruikar <rahul.ruikar@gmail.com>
    [mina86@mina86.com: updated commit message]
    Signed-off-by: Michal Nazarewicz <mina86@mina86.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/f_mass_storage.c
+++ drivers/usb/gadget/f_mass_storage.c
@@ -2846,6 +2846,7 @@ static struct fsg_common *fsg_common_init(struct fsg_common *common,
 		if (rc) {
 			INFO(common, "failed to register LUN%d: %d\n", i, rc);
 			common->nluns = i;
+			put_device(&curlun->dev);
 			goto error_release;
 		}
 
